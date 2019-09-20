1ce05fc6dd5356ff603b525be1e9fbe6fb81c265
Ed Lafargue <ed@wizkers.io>
2019-09-20 00:12:04 -0700

    Initial commit - minimum set of files to get the stack up

---
+++ initialize.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Do not run this again, the files are now configured
+
+#docker run --rm telegraf telegraf config > telegraf.conf
+#docker run --rm influxdb influxd config > influxdb/influxdb.conf
