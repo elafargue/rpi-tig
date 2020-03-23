86af34093d13b306378c57e10bcd076c6829334d
Ed Lafargue <ed@wizkers.io>
2020-03-22 23:25:18 -0700

    Change template field for admin passwordZ

--- start.sh
+++ start.sh
@@ -9,11 +9,11 @@ if [ $(grep -c "PASSWORD_TO_CHANGE" env.influxdb) -ne 0 ]; then
    echo "Warning: this is not enough to consider this installation is secure"
 fi
 
-if [ $(grep -c "ADMIN_PASSWORD" env.influxdb) -ne 0 ]; then
+if [ $(grep -c "ADMIN_TO_CHANGE" env.influxdb) -ne 0 ]; then
    echo "Default password detected for InfluxDB administrator..."
    echo "Setting up a random password for this installation"
    PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
-   sed -i "s/ADMIN_PASSWORD/$PASSWORD/g" env.influxdb
+   sed -i "s/ADMIN_TO_CHANGE/$PASSWORD/g" env.influxdb
    echo "... done"
    echo "Warning: again, the password is in the env.influxdb file now, be careful"
 fi
